80909571416fdcb50dd93371aea166edd831ac76
Tomoya Takinami <hjtakinami@gmail.com>
2024-07-26 17:01:49 +0200

    remove log

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -3,51 +3,49 @@ import type { MicroCMSQueries } from "microcms-js-sdk";
 import { createClient } from "microcms-js-sdk";
 
 // 2. Connect domain & API key from microCMS
-const client = createClient ({
-    serviceDomain: import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN,
-    apiKey: import.meta.env.VITE_MICROCMS_API_KEY
+const client = createClient({
+  serviceDomain: import.meta.env.VITE_MICROCMS_SERVICE_DOMAIN,
+  apiKey: import.meta.env.VITE_MICROCMS_API_KEY,
 });
-
-console.log('API Key:', import.meta.env.VITE_MICROCMS_API_KEY);
 
 // 3. Define type
 export type Blog = {
-    id: string;
-    createdAt: string;
-    updatedAt: string;
-    publishedAt: string;
-    revisedAt: string;
-    title: string;
-    content: string;
-    eyecatch: {
-        url: string;
-        height: number;
-        width: number;
-    }
-}
+  id: string;
+  createdAt: string;
+  updatedAt: string;
+  publishedAt: string;
+  revisedAt: string;
+  title: string;
+  content: string;
+  eyecatch: {
+    url: string;
+    height: number;
+    width: number;
+  };
+};
 
 export type BlogResponse = {
-    totalCount: number;
-    offset: number;
-    limit: number;
-    contents: Blog[];
-}
+  totalCount: number;
+  offset: number;
+  limit: number;
+  contents: Blog[];
+};
 
 // 4. Call API
 export const getBlogs = async (queries?: MicroCMSQueries) => {
-    return await client.get<BlogResponse>({
-        endpoint: "blogs",
-        queries
-    })
+  return await client.get<BlogResponse>({
+    endpoint: "blogs",
+    queries,
+  });
 };
 
 export const getBlogDetail = async (
-    contentId: string,
-    queries?: MicroCMSQueries
+  contentId: string,
+  queries?: MicroCMSQueries
 ) => {
-    return await client.getListDetail<Blog>({
-        endpoint: "blogs",
-        contentId,
-        queries,
-    })
-}
+  return await client.getListDetail<Blog>({
+    endpoint: "blogs",
+    contentId,
+    queries,
+  });
+};
