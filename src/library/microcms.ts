56ba913924ec743d236b0e2ba771c2fd4240b99c
Tomoya Takinami <hjtakinami@gmail.com>
2024-07-26 16:03:58 +0200

    updated

--- src/library/microcms.ts
+++ src/library/microcms.ts
@@ -4,8 +4,8 @@ import { createClient } from "microcms-js-sdk";
 
 // 2. Connect domain & API key from microCMS
 const client = createClient ({
-    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN,
-    apiKey: import.meta.env.MICROCMS_API_KEY
+    serviceDomain: import.meta.env.MICROCMS_SERVICE_DOMAIN || "",
+    apiKey: import.meta.env.MICROCMS_API_KEY || ""
 });
 
 // 3. Define type
