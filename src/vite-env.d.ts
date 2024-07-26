88ebf80886ec5628cc0c2dd47416c6b1af9d66c3
Tomoya Takinami <hjtakinami@gmail.com>
2024-07-26 16:11:58 +0200

    updated env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -3,7 +3,6 @@
 interface ImportMetaEnv {
     readonly MICROCMS_SERVICE_DOMAIN: string;
     readonly MICROCMS_API_KEY: string;
-    // add more environment variables as needed
   }
 
   interface ImportMeta {
